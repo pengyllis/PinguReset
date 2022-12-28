97715d1d427254a45d7ec935b0cde1fe215213f0
Baryllium <baryllium@tutanota.com>
2022-12-28 20:52:09 +0100

    rewrite to auto-setup using a mc screenshot

--- bottomleftsetting.py
+++
@@ -1,6 +0,0 @@
-from pynput.mouse import Controller
-import time
-
-ms = Controller()
-time.sleep(10)
-print("Bottom-left position:",ms.position)
--- pingureset.py
+++ pingureset.py
@@ -1,39 +1,86 @@
+import os
+import time
+from PIL import Image, ImageGrab
+from pynput.keyboard import Controller, Key, Listener
+
 ###### SETTINGS ######
 refreshTime = 0.2
 loadTime = 1
 guiScale = 2
 xRes, yRes = (1920, 1080)
-leftPos, bottomPos = (0, 1079)
+screenshotsDir = "change this"
+resetKey = Key.f6
+actionKey = Key.ctrl_r
 ######################
-
-import time
-from PIL import Image, ImageGrab
-from pynput.keyboard import Controller, Key
-
-def seedSuitable():
-    hasWater = False
-    hasSand = False
-    img = ImageGrab.grab(bbox = None)
-    xGridSize, yGridSize = xRes//120, yRes//120
-    mapSize = 90 * guiScale
-    for x in range(xGridSize):
-        for y in range(yGridSize):
-            if x*120<mapSize and y*120>yRes-mapSize: continue
-            r,g,b = img.getpixel((x*60, y*60))
-            if r*1.2<b and g*1.2<b and r+g+b<500: hasWater = True
-            elif b*1.2<r and g<r and r+g+b>340: hasSand = True
-            if hasWater and hasSand: break
-    return hasWater and hasSand
 
 kb = Controller()
 loadScreen = False
 alreadySearched = False
+actionPressed = False
+windowPos = (0, 0, xRes, yRes)
+mapSize = 90 * guiScale
+mapPos = (0, yRes-mapSize, mapSize, yRes)
+
+def seedSuitable():
+    hasWater = False
+    hasSand = False
+    img = ImageGrab.grab(bbox = windowPos)
+    for x in range(16):
+        for y in range(9):
+            if xRes * x/16 < mapSize and yRes * y/9 > yRes-mapSize: continue
+            r,g,b = img.getpixel((xRes*x//16, yRes*y//9))
+            if r*1.2<b and g*1.2<b and r+g+b<500: hasWater = True
+            elif b*1.2<r and g<r and r+g+b>340: hasSand = True
+            if hasWater and hasSand: break
+    
+    return hasWater and hasSand
+
+def findPosition():
+    screen = ImageGrab.grab(bbox = None)
+    
+    kb.tap(Key.f2)
+    ssList = os.listdir(screenshotsDir)
+    ssList.sort()
+    mcScreenName = screenshotsDir + ssList[-1]
+    mcScreen = Image.open(mcScreenName)
+    if os.path.exists(mcScreenName):
+        os.remove(mcScreenName)
+    xSize, ySize = mcScreen.size
+    topleftPixel = mcScreen.getpixel((0,0))
+
+    # potential perf improvements: check pixels on a 2x2 grid, begin by checking a small region
+    for x in range(xRes - xSize + 1):
+        for y in range(yRes - ySize + 1):
+            if screen.getpixel((x,y)) == topleftPixel:
+                if mcScreen == screen.crop((x, y, x+xSize, y+ySize)):
+                    return (x, y, x+xSize, y+ySize)
+    raise Exception("No Minecraft window has been found")
+
+def on_press(key):
+    global actionPressed, windowPos
+    if not actionPressed and key == actionKey:
+        actionPressed = True
+        if True: # (placeholder) no reset screen in sight
+            windowPos = findPosition()
+            mapPos = (windowPos[0], windowPos[3]-mapSize, windowPos[0]+mapSize, windowPos[3])
+        else:
+            pass
+            # disable resetting for this seed, probably
+        
+def on_release(key):
+    global actionPressed
+    if actionPressed:
+        actionPressed = False
+
+listener = Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
 
 while True:
     time.sleep(refreshTime)
     
-    mapSize = 90 * guiScale
-    img = ImageGrab.grab(bbox = (leftPos, bottomPos-mapSize, leftPos+mapSize, bottomPos+1))
+    img = ImageGrab.grab(bbox = mapPos)
     if img.getpixel((0,0)) == (0,)*3 and img.getpixel((mapSize-1,0)) == (0,)*3 and img.getpixel((0,mapSize-1)) == (0,)*3 and img.getpixel((mapSize-1,mapSize-1)) == (0,)*3:
         loadScreen = True
         if not alreadySearched:
@@ -52,6 +99,6 @@ while True:
     if seedSuitable():
         kb.press(Key.esc)
     else:
-        kb.tap(Key.f6)
+        kb.tap(resetKey)
     
     alreadySearched = True
